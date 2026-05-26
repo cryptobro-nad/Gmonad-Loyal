847530a58409e64fb7d02c56e3d28ecc172fe231
crypto bro <jonaskhan09@gmail.com>
2026-05-27 00:10:29 +0300

    feat: add GmonadWallV2 contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,13 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: { enabled: true, runs: 200 },
+      viaIR: true,
+    },
+  },
   networks: {
     monadTestnet: {
       url: "https://testnet-rpc.monad.xyz",
